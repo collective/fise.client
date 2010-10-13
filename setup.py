9e264726da7cd28609b8637ef453eef7a0cc08bb
Jens W. Klein <jens@bluedynamics.com>
2010-10-13 14:13:06 +0200

    make it fise.client

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
-# Python Software Foundation License
-
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.3.2'
-shortdesc = 'Ordered dictionary.'
-longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+version = '1.0'
+shortdesc = 'FISE Semantic Engine: RESTful Python Client.'
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 tests_require = ['interlude']
 
-setup(name='odict',
+setup(name='fise.client',
       version=version,
       description=shortdesc,
       long_description=longdesc,
       classifiers=[
             'Development Status :: 5 - Production/Stable',
-            'License :: OSI Approved :: Python Software Foundation License',
+            'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
             'Programming Language :: Python', 
             'Topic :: Software Development',       
       ],
-      keywords='odict dict ordered dictionary mapping collection tree',
-      author='Robert Niederreiter',
-      author_email='rnix@squarewave.at',
-      url=u'https://svn.plone.org/svn/archetypes/AGX/odict',
-      license='Python Software Foundation License',
+      keywords='',
+      author='Jens Klein    ',
+      author_email='jens@bluedynamics.com',
+      url=u'',
+      license='Simplifed BSD like',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
       namespace_packages=[],
@@ -31,7 +29,7 @@ setup(name='odict',
       zip_safe=True,
       install_requires=['setuptools'],
       tests_require=tests_require,
-      test_suite="odict.tests.test_suite",
+      test_suite="fise.client.tests.test_suite",
       extras_require = dict(
           test=tests_require,
       ),
